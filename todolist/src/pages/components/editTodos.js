4eafca60a2011278a91698bbc0db47905589ed3f
jian0684@umn.edu <jian0684@umn.edu>
2023-04-16 21:07:35 -0500

    edit

--- todolist/src/pages/components/editTodos.js
+++ todolist/src/pages/components/editTodos.js
@@ -67,3 +67,46 @@ export async function markTodolist(token, state, id) {
     });
     return data;
 }
+
+export async function getTodoitem(token, userid, id) {
+    console.log(userid)
+    const backend = "https://backend-lebq.api.codehooks.io/dev/todolist?" + "userid="+ userid + "&_id=" + id;
+    const access = "af51eaa1-449e-4100-829b-6093eaa3387f";
+
+    const response = await fetch(backend,{
+        'method': 'GET',
+        'headers': {'Authorization': 'Bearer ' + token,
+        'x-apikey': access,
+        'Content-Type': 'application/json'},
+    }).catch(e => {
+        console.log("ERROS")
+        console.log(e)
+    });
+    const data = await response.json().catch(e => {
+        console.log("ERROS")
+        console.log(e)
+    });
+    return data;
+}
+
+
+export async function updateTodo(token, userid, id, todo) {
+    const backend = "https://backend-lebq.api.codehooks.io/dev/todolist/" + id + "?userid="+ userid;
+    const access = "af51eaa1-449e-4100-829b-6093eaa3387f";
+
+    const response = await fetch(backend,{
+        'method': 'PATCH',
+        'headers': {'Authorization': 'Bearer ' + token,
+        'x-apikey': access,
+        'Content-Type': 'application/json'},
+        'body': JSON.stringify({todo: todo}),
+    }).catch(e => {
+        console.log("ERROS")
+        console.log(e)
+    });
+    const data = await response.json().catch(e => {
+        console.log("ERROS")
+        console.log(e)
+    });
+    return data;
+}
--- todolist/src/pages/todos/[id].js
+++ todolist/src/pages/todos/[id].js
@@ -1,12 +1,40 @@
 import { useRouter } from 'next/router'
-import Head
- from 'next/head'
-export default function Home() {
-    const router = useRouter()
-    const { id } = router.query
+import { useAuth } from '@clerk/nextjs';
+import { useState, useEffect } from 'react';
+import { getTodoitem, updateTodo } from '../components/editTodos';
+
+export default function ChangeTodos() {
+  
+  const [task, setTask] = useState("");
+  const router = useRouter();
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+
+
+  const { id } = router.query
+  
+  
+  useEffect(() =>{
+    const update = async () => {
+      if(userId != null){
+        console.log(userId);
+        const token = await getToken({ template: "codehooks" });
+        const res = await getTodoitem(token,userId,id);
+        console.log(res[0].todo);
+        
+        setTask(res[0].todo);
+      }
+       
+    }
+    update().catch(console.error);
+  }, [userId])
+    
   return (
     <>
-    <h1>{id}</h1>
+      <textarea rows="5" cols="50" onChange={(e) => setTask(e.target.value)} defaultValue={task} />
+      <button onClick={ async () => {
+        const token = await getToken({ template: "codehooks" });
+        await updateTodo(token,userId,id,task);
+      }}>Make Change</button>
     </>
   )
 }
